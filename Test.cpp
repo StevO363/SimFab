ce12f9fc432a30d1abe79ace8c848454482a6455
StevO363 <stefan.lenz98@gmail.com>
2024-05-30 15:15:08 +0200

    added vector velocity

--- Test.cpp
+++ Test.cpp
@@ -10,51 +10,73 @@
 #include <lsToSurfaceMesh.hpp>
 #include <lsVTKWriter.hpp>
 
+constexpr int D = 3;
+
 // Implement own velocity field
 class velocityField : public lsVelocityField<double> {
 public:
-  double
-  getScalarVelocity(const std::array<double, 3> & /*coordinate*/, int material,
+  // double
+  std::array<double, D>
+  getVectorVelocity(const std::array<double, 3> & /*coordinate*/, int material,
                     const std::array<double, 3> & /*normalVector*/,
                     unsigned long pointID) final {
-        return 1;
+        std::array<double, D> v = {0, 0, 1};
+        return v;
+        // return 1;
   }
 };
 
 
 int main() {
-    //seting  number of dimensions
-    constexpr int D = 3;
+  //seting  number of dimensions
+  
 
-    //extent of the domain in each direction
-    double extent = 40 ;
-    double gridDelta = 1;
+  //extent of the domain in each direction
+  double extent = 40 ;
+  double gridDelta = 1;
+  double Add = 20;
+  double bounds[2 * D] = {-extent-Add, extent+Add, -extent-Add, extent+Add, -extent-Add, extent+Add};
 
-    double bounds[2 * D] = {-extent, extent, -extent, extent, -extent, extent};
+  // definging the boundary conditions
+  lsDomain<double, D>::BoundaryType boundaryCons[D];
+  boundaryCons[0] = lsDomain<double, D>::BoundaryType::REFLECTIVE_BOUNDARY;
+  boundaryCons[1] = lsDomain<double, D>::BoundaryType::REFLECTIVE_BOUNDARY;
+  boundaryCons[2] = lsDomain<double, D>::BoundaryType::INFINITE_BOUNDARY;
 
-    // definging the boundary conditions
-    lsDomain<double, D>::BoundaryType boundaryCons[D];
-    boundaryCons[0] = lsDomain<double, D>::BoundaryType::REFLECTIVE_BOUNDARY;
-    boundaryCons[1] = lsDomain<double, D>::BoundaryType::REFLECTIVE_BOUNDARY;
-    boundaryCons[2] = lsDomain<double, D>::BoundaryType::INFINITE_BOUNDARY;
+  auto substrate = lsSmartPointer<lsDomain<double, D>>::New(bounds, boundaryCons, gridDelta);
 
-    auto substrate = lsSmartPointer<lsDomain<double, D>>::New(bounds, boundaryCons, gridDelta);
+  double origin[3] = {0, 0, 0};
+  double PlaneNormal[3] = {0, 0, 1};
 
-    double origin[3] = {0, 0, 0};
-    double PlaneNormal[3] = {0, 0, 1};
+  double minCorner[D] = {-extent+1, -extent+1, -40};
+  double maxCorner[D] = {extent-1, extent-1, 0};
 
-    double minCorner[D] = {-extent+1, -extent+1, -40};
-    double maxCorner[D] = {extent-1, extent-1, 0};
+  auto Box = lsSmartPointer<lsBox<double, D>>::New(minCorner,maxCorner);
+  lsMakeGeometry<double, D>(substrate, Box).apply();
 
-    auto Box = lsSmartPointer<lsBox<double, D>>::New(minCorner,maxCorner);
-    lsMakeGeometry<double, D>(substrate, Box).apply();
+  auto mesh = lsSmartPointer<lsMesh<double>>::New();
 
-    auto mesh = lsSmartPointer<lsMesh<double>>::New();
+  std::cout << "Extracting..." << std::endl;
+  lsToSurfaceMesh<double, D>(substrate, mesh).apply();
+  lsVTKWriter<double>(mesh, "Test/substrate-0.vtk").apply();
 
-    std::cout << "Extracting..." << std::endl;
-    lsToSurfaceMesh<double, D>(substrate, mesh).apply();
-    lsVTKWriter<double>(mesh, "Test/substrate-0.vtk").apply();
+  auto mask = lsSmartPointer<lsDomain<double, D>>::New(substrate);
+  auto velocities = lsSmartPointer<velocityField>::New();
 
+  std::cout << "Advecting..." << std::endl;
+  lsAdvect<double, D> advectionKernel;
+  advectionKernel.setVelocityField(velocities);
+  advectionKernel.insertNextLevelSet(mask);
 
-    return EXIT_SUCCESS;
+  advectionKernel.setAdvectionTime(10.);
+  advectionKernel.apply();
+  double advectionSteps = advectionKernel.getNumberOfTimeSteps();
+  std::cout << "Number of Advection steps taken: " << advectionSteps
+            << std::endl;
+
+  std::cout << "Extracting..." << std::endl;
+  lsToSurfaceMesh<double, D>(mask, mesh).apply();
+  lsVTKWriter<double>(mesh, "Test/mask-0.vtk").apply();
+  
+  return EXIT_SUCCESS;
 }
