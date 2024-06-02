f17152c571e04c8e119c288dce844f1ca99ca5f6
StevO363 <stefan.lenz98@gmail.com>
2024-06-03 01:49:06 +0200

    actually finished task2

--- Task2/task2.cpp
+++ Task2/task2.cpp
@@ -9,6 +9,7 @@
 #include <lsToSurfaceMesh.hpp>
 #include <lsVTKWriter.hpp>
 
+// isotropic deposition velocity field
 class passivVelocityField : public lsVelocityField<double> {
     public:
     double getScalarVelocity(const std::array<double, 3> & /*coordinate*/,
@@ -27,12 +28,14 @@ public:
     double velocity;
     switch (material) {
     case 0:
-      velocity = -1; 
+      velocity = -1; //substrate etch isotropically
       break;
     case 1:
-      velocity = 0.; 
+      velocity = 0.; //mask material velocity is 0 (could also be set to be very small doesnt matter)
       break;
-    case 2: 
+    case 2:
+      // velocity for the passivation material it is assumend ALMOST perfectly directional etching
+      // etch rate is the vertical component of surface normal but at least -0.2 (thats the almost perfect)
       velocity = std::min(-std::abs(normalVector[2]), -0.2);    
   }
   return velocity;
@@ -40,115 +43,118 @@ public:
 };
 
 int main(int argc, char** argv){
-    if (argc != 4){
-        std::cout << "Wrong number of Arguments!" << std::endl;
-        std::cout << "./task2 <NumCycles> <DeposTime> <EtchTime>" << std::endl;
-        return EXIT_FAILURE;
-    }
 
-    int NumCycles = std::stoi(argv[1]);
-    double DeposTime = std::stod(argv[2]);
-    double EtchTime = std::stod(argv[3]);
+  // Parsing args
+  if (argc != 4){
+      std::cout << "Wrong number of Arguments!" << std::endl;
+      std::cout << "./task2 <NumCycles> <DeposTime> <EtchTime>" << std::endl;
+      return EXIT_FAILURE;
+  }
 
-    //set up dimensions and domain extent
-    constexpr int D = 3;
-    double padding = 5;
-    double extent = 40;
-    double gridDelta = 1;
+  int NumCycles = std::stoi(argv[1]);
+  double DeposTime = std::stod(argv[2]);
+  double EtchTime = std::stod(argv[3]);
 
-    double bounds[2 * D] = {-extent-padding, extent+padding, -extent-padding, extent+padding, -extent-padding, extent+padding};
-    // definging the boundary conditions
-    lsDomain<double, D>::BoundaryType boundaryCons[D];
-    boundaryCons[0] = lsDomain<double, D>::BoundaryType::REFLECTIVE_BOUNDARY;
-    boundaryCons[1] = lsDomain<double, D>::BoundaryType::REFLECTIVE_BOUNDARY;
-    boundaryCons[2] = lsDomain<double, D>::BoundaryType::INFINITE_BOUNDARY;
+  //set up dimensions and domain extent
+  constexpr int D = 3;
+  double extent = 40;
+  double gridDelta = 1;
 
-    auto substrate = lsSmartPointer<lsDomain<double, D>>::New(bounds, boundaryCons, gridDelta); //creating substrate plane
-    double origin[3] = {0., 0., 0.};
-    double planeNormal[3] = {0., 0., 1.};
-    double minCorner[D] = {-extent*10, -extent*10, -100};
-    double maxCorner[D] = {extent*10, extent*10, 0};
+  double bounds[2 * D] = {-extent, extent, -extent, extent, -extent, extent};
 
-    {
-        auto Box = lsSmartPointer<lsBox<double, D>>::New(minCorner,maxCorner);
-        lsMakeGeometry<double, D>(substrate, Box).apply();
-    }
-    auto mask = lsSmartPointer<lsDomain<double, D>>::New(bounds, boundaryCons, gridDelta);
-    {
-        double minCornerMask[3] = {-extent*10, -extent*10, 0};
-        double maxCornerMask[3] = {extent*10, extent*10, 5};
-        auto Box_mask = lsSmartPointer<lsBox<double, D>>::New(minCornerMask,maxCornerMask);
-        lsMakeGeometry<double, D>(mask, Box_mask).apply();
-    }
+  // definging the boundary conditions
+  lsDomain<double, D>::BoundaryType boundaryCons[D];
+  boundaryCons[0] = lsDomain<double, D>::BoundaryType::REFLECTIVE_BOUNDARY;
+  boundaryCons[1] = lsDomain<double, D>::BoundaryType::REFLECTIVE_BOUNDARY;
+  boundaryCons[2] = lsDomain<double, D>::BoundaryType::INFINITE_BOUNDARY;
 
-    auto Cylinder = lsSmartPointer<lsDomain<double, D>>::New(bounds, boundaryCons, gridDelta);
-    {
-        //reusing the normal and the origin from the plane before
-        lsMakeGeometry<double, D>(Cylinder, lsSmartPointer<lsCylinder<double, D>>::New(origin, planeNormal, 5, 15)).apply();
-    }
+  //substrate plane
+  auto substrate = lsSmartPointer<lsDomain<double, D>>::New(bounds, boundaryCons, gridDelta); 
+  double origin[3] = {0., 0., 0.};
+  double planeNormal[3] = {0., 0., 1.};
 
-    lsBooleanOperation<double, D>(mask, Cylinder, lsBooleanOperationEnum::RELATIVE_COMPLEMENT).apply();
-    
-    {
-        auto mesh = lsSmartPointer<lsMesh<double>>::New();
-        lsToSurfaceMesh<double, D>(mask, mesh).apply();
-        lsVTKWriter<double>(mesh, "VTK/mask_finished.vtk").apply();
-    }
+  {
+      auto Plane = lsSmartPointer<lsPlane<double, D>>::New(origin, planeNormal);
+      lsMakeGeometry<double, D>(substrate, Plane).apply();
+  }
 
-    lsBooleanOperation<double, D>(mask, substrate, lsBooleanOperationEnum::UNION).apply();
+  //mask plane
+  auto mask = lsSmartPointer<lsDomain<double, D>>::New(bounds, boundaryCons, gridDelta);
+  {
+      double origin1[3] = {0, 0, 5};
+      auto Plane_mask = lsSmartPointer<lsPlane<double, D>>::New(origin1, planeNormal);
+      lsMakeGeometry<double, D>(mask, Plane_mask).apply();
+  }
 
-    {
-        std::cout << "Extracting (init state)...." << std::endl;
-        auto mesh = lsSmartPointer<lsMesh<double>>::New();
-        lsToSurfaceMesh<double, D>(substrate, mesh).apply();
-        lsVTKWriter<double>(mesh, "VTK/substrate_w_mask.vtk").apply();
-    }
+  //creating the hole in the mask with a cylinder
+  auto Cylinder = lsSmartPointer<lsDomain<double, D>>::New(bounds, boundaryCons, gridDelta);
+  {
+      //reusing the normal and the origin from the plane before
+      lsMakeGeometry<double, D>(Cylinder, lsSmartPointer<lsCylinder<double, D>>::New(origin, planeNormal, 5, 15)).apply();
+  }
 
-    //creating new lsDomains for the passivation layer
-    auto passivLayer = lsSmartPointer<lsDomain<double, D>>::New(mask);
-    
-    for (int i = 0; i < NumCycles; i++){
-      {
-          std::cout << "Dir Etch ...\n";
-          lsAdvect<double, D> advectionKernel;
-          auto velocities = lsSmartPointer<directionalEtch>::New();
-
-          advectionKernel.setVelocityField(velocities);
-          advectionKernel.insertNextLevelSet(substrate);
-          advectionKernel.insertNextLevelSet(mask);
-          advectionKernel.insertNextLevelSet(passivLayer);
-
-          advectionKernel.setAdvectionTime(EtchTime);
-          advectionKernel.apply();
-      }
-      if (i != NumCycles-1) {
-        {
-          std::cout << "advecting passiv layer ... " << std::endl;
-          lsAdvect<double, D> advectionKernel;
-          auto velocities = lsSmartPointer<passivVelocityField>::New();
-
-          advectionKernel.setVelocityField(velocities);
-          advectionKernel.insertNextLevelSet(substrate);
-          advectionKernel.insertNextLevelSet(mask);
-          advectionKernel.insertNextLevelSet(passivLayer);
-
-          advectionKernel.setAdvectionTime(DeposTime);
-          advectionKernel.apply();
-        } 
-      }
-      std::cout << "completed cycle: " << i+1 << "/" << NumCycles << std::endl;
-    }
+  lsBooleanOperation<double, D>(mask, Cylinder, lsBooleanOperationEnum::RELATIVE_COMPLEMENT).apply();
   
-    auto mesh = lsSmartPointer<lsMesh<double>>::New();
-    lsToSurfaceMesh<double, D>(mask, mesh).apply();
-    lsVTKWriter<double>(mesh, "VTK/mask_finish.vtk").apply();
-    lsToSurfaceMesh<double, D>(substrate, mesh).apply();
-    lsVTKWriter<double>(mesh, "VTK/substrate_finish.vtk").apply();    
-    lsToSurfaceMesh<double, D>(passivLayer, mesh).apply();
-    lsVTKWriter<double>(mesh, "VTK/passivLayer_finish.vtk").apply();
-    
+  {
+      auto mesh = lsSmartPointer<lsMesh<double>>::New();
+      lsToSurfaceMesh<double, D>(mask, mesh).apply();
+      lsVTKWriter<double>(mesh, "VTK/mask_finished.vtk").apply();
+  }
 
+  lsBooleanOperation<double, D>(mask, substrate, lsBooleanOperationEnum::UNION).apply();
 
+  //extract initial state
+  {
+      std::cout << "Extracting (init state)...." << std::endl;
+      auto mesh = lsSmartPointer<lsMesh<double>>::New();
+      lsToSurfaceMesh<double, D>(substrate, mesh).apply();
+      lsVTKWriter<double>(mesh, "VTK/substrate_w_mask.vtk").apply();
+  }
 
-    return EXIT_SUCCESS;
+  //creating new lsDomains for the passivation layer
+  auto passivLayer = lsSmartPointer<lsDomain<double, D>>::New(mask);
+  
+  //for loop to go  through the steps of the Bosch process
+  for (int i = 0; i < NumCycles; i++){
+    {
+        std::cout << "Dir Etch ...\n";
+        lsAdvect<double, D> advectionKernel;
+        auto velocities = lsSmartPointer<directionalEtch>::New();
+
+        advectionKernel.setVelocityField(velocities);
+        advectionKernel.insertNextLevelSet(substrate);
+        advectionKernel.insertNextLevelSet(mask);
+        advectionKernel.insertNextLevelSet(passivLayer);
+
+        advectionKernel.setAdvectionTime(EtchTime);
+        advectionKernel.apply();
+    }
+    if (i != NumCycles-1) {
+      {
+        std::cout << "advecting passiv layer ... " << std::endl;
+        lsAdvect<double, D> advectionKernel;
+        auto velocities = lsSmartPointer<passivVelocityField>::New();
+
+        advectionKernel.setVelocityField(velocities);
+        advectionKernel.insertNextLevelSet(substrate);
+        advectionKernel.insertNextLevelSet(mask);
+        advectionKernel.insertNextLevelSet(passivLayer);
+
+        advectionKernel.setAdvectionTime(DeposTime);
+        advectionKernel.apply();
+      } 
+    }
+    std::cout << "completed cycle: " << i+1 << "/" << NumCycles << std::endl;
+  }
+
+  //extracting the final geometry
+  auto mesh = lsSmartPointer<lsMesh<double>>::New();
+  lsToSurfaceMesh<double, D>(mask, mesh).apply();
+  lsVTKWriter<double>(mesh, "VTK/mask_finish.vtk").apply();
+  lsToSurfaceMesh<double, D>(substrate, mesh).apply();
+  lsVTKWriter<double>(mesh, "VTK/substrate_finish.vtk").apply();    
+  lsToSurfaceMesh<double, D>(passivLayer, mesh).apply();
+  lsVTKWriter<double>(mesh, "VTK/passivLayer_finish.vtk").apply();
+  
+  return EXIT_SUCCESS;
 }
